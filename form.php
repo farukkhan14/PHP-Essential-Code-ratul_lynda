e575db4e6db4530d1ba4c394a376dcc3a9755908
farukkhan14 <faruk.cse14@gmail.com>
2015-09-09 02:26:50 +0600

    code

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+	<form action="form_processing.php" method="post">
+			username: <input type="text" name="username" value=""><br><br>
+			password: <input type="password" name="password" value=""><br>
+			<br>
+			<input type="submit" name="submit" value="Submit">
+
+	</form>
+</body>
+</html>
